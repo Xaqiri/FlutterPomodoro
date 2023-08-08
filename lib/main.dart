7fb040adfaf483dd733cc8f44b579b391e11b82e
Xaqiri <xaqiri@outlook.com>
2023-08-08 13:34:12 -0500

    Added single countdown timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,158 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+enum timerOptions { start, stop }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pomodoro',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainView(title: 'Pomodoro'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MainView extends StatefulWidget {
+  const MainView({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainView> createState() => _MainViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainViewState extends State<MainView> {
+  int _minutes = 1;
+  String _minutesDisplay = "1";
+  int _seconds = 0;
+  String _secondsDisplay = "00";
+  Timer? t;
+  Icon _timerButtonIcon = const Icon(Icons.play_arrow);
+  timerOptions _timerAction = timerOptions.start;
+  var _tick = 0;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _decrementMinutes() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _minutes--;
+      _minutesDisplay = _minutes.toString();
+      _seconds = 0;
+      setTimerDisplay();
+    });
+  }
+
+  void _incrementMinutes() {
+    setState(() {
+      _minutes++;
+      _minutesDisplay = _minutes.toString();
+      _seconds = 0;
+      setTimerDisplay();
+    });
+  }
+
+  void _startTimer() {
+    void tFunc(Timer timer) {
+      setState(() {
+        _tick++;
+        _timerButtonIcon = const Icon(Icons.stop);
+        _timerAction = timerOptions.stop;
+        _seconds--;
+        if (_seconds < 0) {
+          _seconds = 59;
+          _minutes--;
+        }
+        setTimerDisplay();
+      });
+      if (_minutes == 0 && _seconds == 0) {
+        timer.cancel();
+      }
+      if (_tick == 1) {
+        timer.cancel();
+      }
+    }
+
+    t = Timer.periodic(const Duration(seconds: 0), tFunc);
+    t = Timer.periodic(const Duration(seconds: 1), tFunc);
+  }
+
+  void _stopTimer() {
+    setState(() {
+      t!.cancel();
+      _tick = 0;
+      _timerButtonIcon = const Icon(Icons.play_arrow);
+      _timerAction = timerOptions.start;
+    });
+  }
+
+  void setTimerDisplay() {
+    setState(() {
+      _minutesDisplay = "$_minutes";
+      _seconds < 10
+          ? _secondsDisplay = "0$_seconds"
+          : _secondsDisplay = "$_seconds";
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Work Timer',
             ),
             Text(
-              '$_counter',
+              '$_minutesDisplay:$_secondsDisplay',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FilledButton(
+                  onPressed: _decrementMinutes,
+                  child: const Icon(Icons.remove),
+                ),
+                FilledButton(
+                  onPressed: _incrementMinutes,
+                  child: const Icon(Icons.add),
+                ),
+              ],
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: switch (_timerAction) {
+          timerOptions.start => _startTimer,
+          timerOptions.stop => _stopTimer
+        },
+        tooltip: switch (_timerAction) {
+          timerOptions.start => "Start timer",
+          timerOptions.stop => "Stop timer"
+        },
+        child: _timerButtonIcon,
+      ),
     );
   }
 }
